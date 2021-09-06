36b8e2eec8022856296dac6b38063e7669c612ba
Carlo Cardella <Carloc@microsoft.com>
2021-09-06 08:38:45 -0700

    update package

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('vscode-changelogmanager.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-ChangelogManager!');
+		// vscode.window.showInformationMessage('Hello World from vscode-ChangelogManager!');
 	});
 
 	context.subscriptions.push(disposable);
